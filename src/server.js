6630ad0498046ae35ce868e39f2d377876f00d19
Ayush771369 <ayusharch.77@gmail.com>
2026-05-20 23:20:36 +0530

    added db test route

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
+import prisma from "./config/prisma.client.js";
+
+
 import dotenv from "dotenv";
+
 dotenv.config();
 import app from "./app.js";
 import { logger } from "./utils/logger.js";
@@ -15,4 +19,23 @@ console.log("Server database url: ", process.env.DATABASE_URL);
 
 app.get("/", (req, res) => {
     res.send("Notification backend is running 🚀");
+});
+
+app.get("/test-db", async (req, res) => {
+    try {
+        await prisma.$connect();
+
+        res.json({
+            success: true,
+            message: "Database connected successfully 🚀"
+        });
+
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
 });
