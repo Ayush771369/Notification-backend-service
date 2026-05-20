b577bb5e5682fea2b408790d53f1508f420991db
Ayush771369 <ayusharch.77@gmail.com>
2026-05-20 23:25:47 +0530

    fixed route

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
 import prisma from "./config/prisma.client.js";
 
-
 import dotenv from "dotenv";
-
 dotenv.config();
+
 import app from "./app.js";
 import { logger } from "./utils/logger.js";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  logger.info(`Server is running on port ${PORT}`);
-});
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is healthy' });
 });
+
 console.log("Server database url: ", process.env.DATABASE_URL);
 
 app.get("/", (req, res) => {
@@ -38,4 +35,8 @@ app.get("/test-db", async (req, res) => {
             error: error.message
         });
     }
+});
+
+app.listen(PORT, () => {
+  logger.info(`Server is running on port ${PORT}`);
 });
